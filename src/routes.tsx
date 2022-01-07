024d066fcfa5499066cc6daca8b1fc9d47bc0152
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2022-01-07 13:38:21 -0300

    Adiciona o switch nas routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Artigo } from './pages/Artigo';
@@ -10,12 +10,14 @@ import { PageNotFound } from './pages/PageNotFound';
 function Routes() {
   return(
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/artigos" exact component={Artigo} />
-      <Route path="/blog" exact component={Blog} />
-      <Route path="/contato" exact component={Contato} />
-      <Route path="/portifolio" exact component={Portifolio} />
-      <Route component={PageNotFound} />
+      <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/artigos" component={Artigo} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contato" component={Contato} />
+          <Route path="/portifolio" component={Portifolio} />
+          <Route component={PageNotFound} />
+      </Switch>
     </BrowserRouter>
   )
 }
