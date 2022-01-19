a014a5f59cbe4565105b7cfa2930bf648b287c51
alexanderiscoding <94754185+alexanderiscoding@users.noreply.github.com>
2022-01-19 13:57:07 -0300

    Finaliza Blog

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,9 +1,41 @@
 import { App } from "../../App";
+import { Container,
+        ContainerFirst,
+        ContainerCard,
+        ContainerCardImg,
+        ContainerCardContent,
+        ContainerSecond,
+        ContainerTextCenterMenu } from './styles';
+
+const listPosts = [
+    { id: "0", img: "https://blog.hubspot.com/hubfs/GettyImages-974683580.jpg", title: "Criando o seu site de Portifólio somente com HTML & CSS", subtitle: "Faça seu site de forma rápida e simples com essas incriveis dicas.", category: "HTML & CSS" },
+];
 
 export function Blog() {
     return (
         <App>
-            <h1> Página de Blog </h1>
+            <Container>
+                <ContainerFirst>
+                {listPosts.map(({ id, img, title, subtitle, category }) => (
+                    <li key={id}>
+                        <ContainerCard>
+                            <ContainerCardImg><img src={img} alt="Thumbnail do Post" /></ContainerCardImg>
+                            <ContainerCardContent>
+                                <h2>{title}</h2>
+                                <h4>{subtitle}</h4>
+                            </ContainerCardContent>
+                            <p>{category}</p>
+                        </ContainerCard>
+                    </li>
+                ))}
+                </ContainerFirst>
+                <ContainerSecond>
+                    <ContainerTextCenterMenu><h3>Categorias</h3></ContainerTextCenterMenu>
+                    <p>HTML & CSS (8)</p><p>Javascript (5)</p><p>React (9)</p><p>Typescript (4)</p><p>React Native (1)</p><p>Outros (2)</p>
+                    <ContainerTextCenterMenu><h3>Tags</h3></ContainerTextCenterMenu>
+                    <p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p><p>Abacaxi</p>
+                </ContainerSecond>
+            </Container>
         </App>
     )
 }
---
+++ src/pages/Blog/styles.ts
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+    margin-right: auto!important;
+    margin-left: auto!important;
+    flex: 0 0 auto;
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const ContainerFirst = styled.div`
+    width: 80%;
+    li {
+        display: flex;
+        padding: 1rem;
+        box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.22);
+        cursor: pointer;
+        transition: 0.4s;
+    }
+
+    li:hover {
+        transform: scale(0.9, 0.9);
+        box-shadow: 5px 5px 30px 15px rgba(0,0,0,0.25), 
+          -5px -5px 30px 15px rgba(0,0,0,0.22);
+    }
+    
+    @media (min-width: 40rem) {
+        li {
+            width: 50%;
+        }
+    }
+    
+    @media (min-width: 56rem) {
+        li {
+            width: 33.3333%;
+        }
+    }      
+`;
+
+export const ContainerCard = styled.div`
+    background: ${props => props.theme.title === 'light' ? props.theme.colors.light_blue : props.theme.colors.dark_blue};
+    border-radius: 0.25rem;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    border-bottom: 5px solid #08FDD8;
+    p {
+        color: #08FDD8;
+        padding: 0.5rem;
+    }
+`;
+
+export const ContainerCardImg = styled.div`
+    padding: 0.5rem;
+    img {
+        height: auto;
+        max-width: 100%;
+        vertical-align: middle;
+        border-radius: 20px;
+    }
+`;
+
+export const ContainerCardContent = styled.div`
+    padding: 1rem;
+    h2 {
+        margin-bottom: 10px;
+    }
+`;
+
+export const ContainerSecond = styled.div`
+    width: 20%;
+    background: ${props => props.theme.title === 'light' ? props.theme.colors.light_blue : props.theme.colors.dark_blue};
+    border-radius: 20px;
+    p {
+        margin-bottom: 5px;
+        margin-left: 15px;
+    }
+    div:first-child {
+        margin-top: 15px !important;
+    }
+    p:last-child {
+        margin-bottom: 25px;
+    }
+`;
+
+export const ContainerTextCenterMenu = styled.div`
+    text-align: center;
+    margin-bottom: 15px;
+    margin-top: 25px;
+`;
